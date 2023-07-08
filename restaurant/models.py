ecc3a0070545c0a9933e594ea8157f3291074e2a
Robel Kebede <robel.kebede@bison.howard.edu>
2023-07-08 23:50:36 +0300

    Updated models for Little Lemon

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,19 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
 
+    class Meta:
+        verbose_name = 'Menu'
+        verbose_name_plural = 'Menu Items'
+
+    def __str__(self) -> str:
+        return f'{self.title} : {str(self.price)}'
+    
 class Booking(models.Model):
-    Name = models.CharField(max_length=255)
-    No_of_guests = models.IntegerField()
-    BookingDate = models.DateTimeField()
-    
-    def __str__(self):
-        return self.Name
-    
-class MenuItems(models.Model):
-    Title = models.CharField(max_length=255)
-    Price = models.DecimalField(max_digits=10, decimal_places=2)
-    Inventory = models.IntegerField()
-    
-    def __str__(self):
-        return self.Title
+    name = models.CharField(max_length=255)
+    number_of_guests = models.IntegerField()
+    booking_date =models.DateTimeField()
+
+    class Meta:
+        verbose_name = 'Booking'
+        verbose_name_plural = 'Booking Records'
+
+    def __str__(self) -> str:
+        return f'{self.name} for {self.number_of_guests} guests on {self.booking_date}'
+

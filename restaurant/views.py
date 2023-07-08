6a4171a2da2732dd885b88a4f44a103b92d7696a
Robel Kebede <robel.kebede@bison.howard.edu>
2023-07-08 10:21:39 +0300

    Created more views with authentication

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,15 +3,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Booking, MenuItems
 from .serializers import BookingSerializer, MenuItemSerializer, UserSerializer
-
+from django.shortcuts import get_object_or_404
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.http import JsonResponse
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
-from rest_framework import permissions, viewsets, generics
+from rest_framework import viewsets, generics
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class BookingView(APIView):
@@ -21,6 +26,7 @@ class BookingView(APIView):
         return Response(serializer.data)
     
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated] 
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     
@@ -34,9 +40,30 @@ class MenuView(APIView):
         
         
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = MenuItems.objects.all()
     serializer_class = MenuItemSerializer
 
-class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
-    queryset = MenuItems.objects.all()
-    serializer_class = MenuItemSerializer
+class SingleMenuItemView(View):
+    @csrf_exempt
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, pk):
+        menu_item = get_object_or_404(MenuItems, pk=pk)
+        data = {
+            'id': menu_item.pk,
+            'title': menu_item.Title,
+        }
+        return JsonResponse(data)
+
+    def put(self, request, pk):
+        menu_item = get_object_or_404(MenuItems, pk=pk)
+        menu_item.Title = request.POST.get('name')
+        menu_item.save()
+        return JsonResponse({'message': 'Menu item updated successfully'})
+
+    def delete(self, request, pk):
+        menu_item = get_object_or_404(MenuItems, pk=pk)
+        menu_item.delete()
+        return JsonResponse({'message': 'Menu item deleted successfully'})

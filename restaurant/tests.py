e0e4e46c24600283b7e7fc65cf861bfdbdf0a682
Robel Kebede <robel.kebede@bison.howard.edu>
2023-07-08 16:42:55 +0300

    Added MenuTest for Menu model

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import MenuItems
 
-# Create your tests here.
+
+class MenuTest(TestCase):
+    def test_get_item(self):
+        item = MenuItems.objects.create(Title="IceCream", Price=80, Inventory=100)
+        self.assertEqual(str(item), "IceCream")

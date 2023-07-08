b3cab459fed236dff088bc789ac8dea742fa90b0
Robel Kebede <robel.kebede@bison.howard.edu>
2023-07-08 23:51:02 +0300

    Updated serializers for models

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,19 +1,25 @@
 from rest_framework import serializers
-from .models import MenuItems, Booking
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
-class MenuItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MenuItems
-        fields = '__all__'
-        
+from .models import Menu, Booking
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Menu
+        fields = ['id','title','price','inventory']
         
 class BookingSerializer(serializers.ModelSerializer):
-    class Meta:
+    class Meta():
         model = Booking
         fields = '__all__'
-        
+
+class GroupNameField(serializers.RelatedField):
+    def to_representation(self, value):
+        # Return the group name
+        return value.name
+
 class UserSerializer(serializers.ModelSerializer):
+    groups = GroupNameField(many=True, read_only=True)
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ('url', 'username', 'email', 'groups')

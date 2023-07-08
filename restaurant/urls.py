e037bb23d0eedc960fdb19c2600d0540d364b6a5
Robel Kebede <robel.kebede@bison.howard.edu>
2023-07-08 23:51:45 +0300

    Updated url tree

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path, include
-from .views import MenuView, BookingView, MenuItemsView, SingleMenuItemView, UserViewSet, BookingViewSet
-from rest_framework import routers
-from rest_framework.authtoken.views import obtain_auth_token
 
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-routerr = routers.DefaultRouter() 
-routerr.register(r'tables', BookingViewSet)
+from . import views
+
+from rest_framework.routers import DefaultRouter
+
+router: DefaultRouter = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
-    path("menu/", MenuView.as_view()), 
-    path("booking/", BookingView.as_view()),
-    path("menu-items/", MenuItemsView.as_view()),
-    path('menu-items/<int:pk>', SingleMenuItemView.as_view()),
-    path('urls/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('bookings /', include(routerr.urls)),
-    path('api-token-auth', obtain_auth_token),
+    path('', views.index, name='home'),
+    path('about/', views.about, name="about"),
+    path('menu/', views.MenuItemsView.as_view(), name='menu'),
+    path('reservations/', views.reservations, name="reservations"),
+    # API paths
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menu-item'),
+    path('booking/', include(router.urls)),
 ]
+
+

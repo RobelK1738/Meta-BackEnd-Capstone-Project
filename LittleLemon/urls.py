5cf159d0e01824acb3efd1a002e2fe90d16d5d04
Robel Kebede <robel.kebede@bison.howard.edu>
2023-07-09 20:39:18 +0300

    Updated url tree

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework.authtoken.views import obtain_auth_token #import obtain_auth_token view
+from rest_framework.authtoken.views import obtain_auth_token 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('restaurant.urls')),
-    path('auth/', include('djoser.urls')),
+    path('admin/', admin.site.urls), # Admin Page
+    path('', include('website.urls')), # API Page
+    path('api/', include('api.urls')), # Website Page
+    path('auth/', include('djoser.urls')), 
     path('auth/', include('djoser.urls.authtoken')),
     path('api-token-auth/', obtain_auth_token),
 ]
